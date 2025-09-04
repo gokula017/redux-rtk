0ece7f514ccb3b302558a9c840280790fc730afd
gokula017 <gokula017@gmail.com>
2025-09-04 16:22:49 +0530

    Update deployment details

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/redux-rtk/", // Replace with your repository name
   plugins: [react()],
 })
